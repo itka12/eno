6536dcc1ea6283c5811ac3ea4efcd3b09274ec63
itka12 <100894352+itka12@users.noreply.github.com>
2023-11-16 14:43:10 +0900

    Add files via upload

---
+++ 11week2/11week2/DynamicArray.cpp
@@ -0,0 +1,28 @@
+#include "DynamicArray.h"
+#include "MyExc.h"
+DynamicArray::DynamicArray(int arraySize) {
+	arr = new int[arraySize];
+
+	size = arraySize;
+}
+DynamicArray::~DynamicArray() {
+	delete[] arr;
+	arr = 0;
+}
+void DynamicArray::SetAt(int index, int value) {
+	if (index < 0 || index >= index)
+		throw MyExc(this, "Out of Range!!!", index);
+		/*return false;*/
+	arr[index] = value;
+	/*return true;*/
+}
+
+int DynamicArray::GetAt(int index) const {
+	return arr[index];
+	if (index < 0 || index >= GetSize())
+		throw MyExc(this, "Out of Range!!!", index);
+	return arr[index];
+}
+int DynamicArray::GetSize() const {
+	return size;
+}
